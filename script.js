9ec84342ab087c9a209da10ec7db3747bfc86e72
venkatesh-na <79442047+venkatesh-na@users.noreply.github.com>
2022-07-21 18:57:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 var input = document.querySelector("input");
-var button = document.querySelector("button")
-var resultDiv = document.querySelector(".result")
+var button = document.querySelector("button");
+var resultDiv = document.querySelector(".result");
 
-button.addEventListener("click",(event)=>{
+button.addEventListener("click",function(event){
     event.preventDefault(); 
     button.textContent = "loading...";
-
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=5d2aba5a660006ca5936ff57f5905f9e`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=5d2aba5a660006ca5936ff57f5905f9e&units=metric`)
     .then(function(res){
         return res.json();
     })
@@ -16,7 +15,7 @@ button.addEventListener("click",(event)=>{
         resultDiv.innerHTML = `
         <div>
             <div>
-                <p>${Math.round(data.main.temp-273.15)}<sup>o</sup></p>
+                <p>${Math.floor(data.main.temp)}<sup>o</sup>C</p>
                 <img src = "https://openweathermap.org/img/w/${data.weather[0].icon}.png"/>
             </div>
             <h2>${value}</h2>
